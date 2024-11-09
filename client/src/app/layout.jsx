0a9dbf8605f1d31d9d67a6cc514b6905f3561547
Vaishnavi <vaishnavitiwari104@gmail.com>
2024-11-09 15:31:21 +0530

    lang

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,10 +1,25 @@
+"use client"
 import { AuthContextProvider } from "@/context/AuthContext";
 import "./globals.css";
 import { UserProvider } from "@/context/UserContext";
 import { SocketContextProvider } from "@/context/SocketContext";
 import { StoreContextProvider } from "@/context/storeContext";
-
+import { useEffect } from "react";
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://translate.google.com/translate_a/element.js?cb=loadGoogleTranslate";
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.loadGoogleTranslate = () => {
+      new window.google.translate.TranslateElement(
+        { pageLanguage: "en", layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
+        "google_translate_element"
+      );
+    };
+  }, []);
+
   return (
     <html lang="en">
       <body>
@@ -17,6 +32,12 @@ export default function RootLayout({ children }) {
             </SocketContextProvider>
           </UserProvider>
         </AuthContextProvider>
+
+
+        
+        <div className="text-sm text-center  text-black rounded-lg bg-gray-200 border p-2 shadow-lg">
+          <div id="google_translate_element" className="inline mb-12">Change Language</div>
+        </div>
       </body>
     </html>
   );
